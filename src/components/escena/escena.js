506c9385368e2c0cff0cc96d11ea1e9b4be12f3e
Martí Arnau <marnauortega@gmail.com>
2022-11-22 12:22:17 +0100

    n1 - exercice 3

--- src/components/escena/escena.js
+++ src/components/escena/escena.js
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+
 export const Escena = (props) => {
-  return <p>{props.content}</p>;
+  return <StyledButton>{props.content}</StyledButton>;
 };
+
+const StyledButton = styled.p`
+  border: 2px solid black;
+  border-radius: 40px;
+  text-align: center;
+  padding: 20px 0;
+`;
